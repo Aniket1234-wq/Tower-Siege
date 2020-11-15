84f5c25747839b3c9db76e0e91e8b7355fe6489f
Aniket1234-wq <71884021+Aniket1234-wq@users.noreply.github.com>
2020-11-15 08:23:38 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -3,15 +3,19 @@ class Ground {
     var options = {
         isStatic: true
     }
-    this.body = Bodies.rectangle(x,y,width,height,options);
-    this.width = width;
-    this.height = height;
-    World.add(world, this.body);
+    this.ground = Bodies.rectangle(x,y,width,height,options);
+    this.x = x;
+    this.y = y;
+   this.width = width;
+   this.height = height;
+    World.add(world, this.ground);
   }
   display(){
-    var pos =this.body.position;
+
+
+
     rectMode(CENTER);
     fill("brown");
-    rect(pos.x, pos.y, this.width, this.height);
+    rect(this.x, this.y, width, height);
   }
 };
--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
 
 var box1, ground1, polygon;
+
+
 
 function setup() {
   createCanvas(800,400);
   var canvas = createCanvas(1200,400);
+
+  engine = Engine.create();
+	world = engine.world;
    
    ground1 = new Ground(100, 100, 20, 100); 
+
+   Engine.run(engine);
 
 }
 
@@ -13,6 +25,6 @@ function draw() {
   background(255,255,255); 
 
     
-  ground.display(); 
+  ground1.display(); 
   drawSprites();
 }
